10064b0a5cdf6e3e243e93bc61afcd8551b16250
Miguel Martin <miguel.martin7.5@hotmail.com>
2013-01-24 00:16:56 +1030

    Moved from BitBucket to GitHub

---
+++ README.md
@@ -0,0 +1,19 @@
+# Anax RTTI
+
+## BRIEF
+The Anax RTTI library is designed for to be used as an alternative to RTTI. You should only use most of, if not all, of these library features if your class depends on virtual functions and objects of the class will change during run-time.
+
+## FEATURES
+1. Object creation through strings
+2. Fast class information and comparrision
+
+### NOTES
+1. Examples are included, please refer to them.
+2. This library _is_ C++03 compatible, but the examples are written with C++11.
+
+## INSPIRATION
+The creation through strings is inspired from [this](http://gamedev.stackexchange.com/questions/17746/entity-component-systems-in-c-how-do-i-discover-types-and-construct-component) question in the stackexchange gamedev community.
+
+## AUTHOR
+
+Miguel Martin - Lead programmer/designer
---
+++ examples/IdentifiableWithClassRegister.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+#include "anax/rtti.h"
+
+/********************************************************************************
+ * GOAL:
+ * To create a random amount of objects of different types, randomly.
+ *
+ * Please do note, that this example requires C++11's initializer list
+ * features.
+ *
+ * Here is some example output, of the program.
+ *
+ * Creating 14 different animals of the following random types:
+ * - Cat
+ * - Dog
+ * - Lion
+ * - Tiger
+ * Starting to create Animals...
+ * Cat
+ * Lion
+ * Lion
+ * Dog
+ * Dog
+ * Lion
+ * Dog
+ * Tiger
+ * Dog
+ * Tiger
+ * Tiger
+ * Cat
+ * Dog
+ * Tiger
+ * Finished creating Animals...
+ * TOOK: 0.037234ms
+ *********************************************************************************/
+
+// An abstract base-class for an Animal
+class Animal
+{
+    ANAX_IDENTIFIABLE_BASE(Animal)
+    
+public:
+    virtual ~Animal() {}
+};
+
+// sub classes
+
+class Cat : public Animal
+{
+    ANAX_IDENTIFIABLE(Cat)
+};
+
+class Dog : public Animal
+{
+    ANAX_IDENTIFIABLE(Dog)
+};
+
+class Lion : public Animal
+{
+    ANAX_IDENTIFIABLE(Lion)
+};
+
+class Tiger : public Animal
+{
+    ANAX_IDENTIFIABLE(Tiger)
+};
+
+// register the sub classes
+ANAX_CLASS_REGISTER(Animal, Cat);
+ANAX_CLASS_REGISTER(Animal, Dog);
+ANAX_CLASS_REGISTER(Animal, Lion);
+ANAX_CLASS_REGISTER(Animal, Tiger);
+
+// generates a new random animal
+Animal* generateRandomAnimal(const std::vector<std::string>& possibleRandomElements)
+{
+    return ac::create<Animal>(possibleRandomElements[rand() % possibleRandomElements.size()]);
+}
+
+double GetTimeNow()
+{
+	using std::chrono::duration;
+	using std::chrono::duration_cast;
+	using std::chrono::high_resolution_clock;
+	
+	return duration_cast<duration<double, std::ratio<1> > >(high_resolution_clock::now().time_since_epoch()).count();
+}
+
+void printTimeTaken(double start, double end)
+{
+	std::cout << "Took: " << (end - start) << " seconds\n";
+}
+
+int main(int argc, char* argv[])
+{
+    const int MAX_AMOUNT_OF_ANIMALS_TO_CREATE = 30;
+    
+    // seed random
+    srand((unsigned int)time(NULL));
+    
+    const int amountOfAnimalsToCreate = rand() % MAX_AMOUNT_OF_ANIMALS_TO_CREATE + 1;
+    
+    // list of possible animals to generate
+    std::vector<std::string> randomAnimalList = { "Cat", "Dog", "Lion", "Tiger" };
+    
+    std::cout << "Creating " << amountOfAnimalsToCreate << " different animals of the following random types:\n";
+    
+    for(auto& x : randomAnimalList)
+    {
+        std::cout << "- " << x << '\n';
+    }
+    
+    std::cout << "Starting to create Animals...\n";
+    
+    double start = 0;
+	double end = 0;
+	
+	start = GetTimeNow();
+    for(size_t i = 0; i < amountOfAnimalsToCreate; ++i)
+    {
+        // generate a new random animal
+        Animal* randomAnimal = generateRandomAnimal(randomAnimalList);
+		
+        // print out the random animal's name
+        // (using the class information)
+        std::cout << randomAnimal->getClass().getName() << '\n';
+        
+        // destroy the animal (good practice)
+        anax::destroy(randomAnimal);
+    }
+    end = GetTimeNow();
+    
+    std::cout << "Finished creating Animals...\n";
+	printTimeTaken(start, end);
+    
+    return 0;
+}
